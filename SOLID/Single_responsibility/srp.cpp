a640c3e1751d519b625ab6d577fe05c9c715708b
Hamilton Tobon <hamiltontobon77@gmail.com>
2019-07-22 16:28:25 +0000

    Single Responsibility Principle

--- SOLID/Single_responsibility/srp.cpp
+++ SOLID/Single_responsibility/srp.cpp
@@ -11,25 +11,48 @@ struct Journal {
   std::string title;
   std::vector<std::string> entries;
 
-  Jorunal(const std::string& title) : title(title) {}
+  explicit Journal(const std::string& title) : title(title) {}
 
   void add_entry(const std::string& entry) {
     static int count = 1;
     entries.push_back(lexical_cast<std::string>(count++) + ": " + entry);
   }
 
+  // Using this function of saving an entry to a file (persistence) is a bad
+  // practice. If you use this functionality of PERSISTENCE in more classes,
+  // and you change the way of how your persistence works, you will have to
+  // change this function 'save' here and in every single class you are
+  // using persistence.
+  // The solution to this is to create another class that handles all your
+  // persistence stuff.
   void save(const std::string& filename) {
     std::ofstream ofs(filename);
     for (auto& e : entries)
-      ofs << e << endl;
+      ofs << e << std::endl;
+  }
+};
+
+struct PersistenceManager {
+  // Only here you can change your persistence function.
+  static void save(const Journal& j, const std::string& filename) {
+    std::ofstream ofs(filename);
+    for (auto& e : j.entries)
+      ofs << e << std::endl;
   }
 };
 
 int main() {
 
-  Journal jorunal{"Dear diary"};
+  Journal journal{"Dear diary"};
   journal.add_entry("I ate a bug");
   journal.add_entry("I cried today");
-  gecthar();
+
+  // Bad practice
+  // journal.save("diary.txt");
+
+  // Good practice
+  PersistenceManager pm;
+  pm.save(journal, "diary.txt");
+
   return 0;
 }
